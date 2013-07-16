d9a87e6145575d0ee8407fc0b491f9116efe9cd9
Jie Shen <jieshen.sjtu@gmail.com>
2013-07-16 20:29:49 +0800

    add vlfeat

--- Gist_CPP/gistGabor.h
+++
@@ -1,9 +0,0 @@
-#ifndef gGabor_H
-#define gGabor_H
-#include "myImg.h"
-#include "singleImg.h"
-
-void gistGabor(myImg gImg,int w,double ***G,int n,int Nfilters,double **&g);
-void downN(singleImg x,int NN,double** &);
-void getnxny(int h,int w,int num);
-#endif
